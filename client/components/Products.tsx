c0b25a75ab8802c71f80fd7a03dad39f3cac9b6b
PRSMendis <rayhan.mendis@gmail.com>
2023-11-09 10:36:42 +1100

    add new product is creating a new item

--- client/components/Products.tsx
+++ client/components/Products.tsx
@@ -36,6 +36,8 @@ const Products: FC<ProductsProps> = () => {
         ]
     }])
 
+    
+
     useEffect(() => {
         const fetchData = async () => {
           const response = await fetch("http://localhost:3000/products?limit=8");
@@ -46,11 +48,28 @@ const Products: FC<ProductsProps> = () => {
         fetchData();
       }, []);
 
+      const handleClick = async () => {
+        const res = await fetch('http://localhost:3000/products', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            title: 'New Product',
+            price: 100
+          })
+        });
+        const data = await res.json();
+        console.log(data);
+      };
+
+    
+
 return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
       <div className="flex justify-between items-center mb-6">
         <h1 className="text-2xl font-semibold">Products</h1>
-        <Button onClick={()=> console.log('test')} variant="secondary">Add New Product</Button>
+        <Button onClick={handleClick} variant="secondary">Add New Product</Button>
       </div>
       <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {products.map((product) => {
